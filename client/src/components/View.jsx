a85504483d18eace21ce2263c986103e4559ed01
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-03-12 16:43:36 -0500

    Update View.jsx

--- client/src/components/View.jsx
+++ client/src/components/View.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const View = ({element, character}) => {
+const View = ({element}) => {
 
     const imageSize = 'standard_fantastic'
     const navigate = useNavigate();
