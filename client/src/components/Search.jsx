48292f7043a12e3b9f36969bd625f0160486e3e1
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-03-16 12:38:33 -0500

    Update Search.jsx

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useState } from 'react'
 import View from './View'
 import { Link } from 'react-router-dom'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
 const Search = () => {
 const [name, setName] = useState("")
@@ -11,34 +11,36 @@ const [characterdata, setCharacterData] = useState({})
 
 
 
-  useEffect(()=>{
-  const fetchData = async()=>{
+//   useEffect(()=>{
+//   const fetchData = async()=>{
+//   const response = await axios.post('https://marvel-api-65d1.onrender.com/', {name})
+//   setCharacterData(response.data)
+// }
+// if (name){
+//   fetchData()
+// }
+
+//   }, [name])
+
+const handleSubmit= async(e)=> {
+    e.preventDefault();
+
   const response = await axios.post('https://marvel-api-65d1.onrender.com/', {name})
-  setCharacterData(response.data)
+
 }
-if (name){
-  fetchData()
-}
-
-  }, [name])
-
-
-
 
   return (
     <>
+    <form onSubmit={handleSubmit}>
     <div className='w-full h-full text-center p-5 cursor-pointer'>
      <Link to = 'https://marvel-characters.onrender.com/'><h1 className='p-5 text-[7.5vmin] font-bold'>MARVEL CHARACTERS</h1></Link> 
-       
-            <input className='border border-white rounded-md' onChange={e=>setName(e.target.value)} type = 'text'  placeholder='Character...' required/>
-          
-       
-        
-        <View element = {characterdata.data} character = {name} />
+    <input className='border border-white rounded-md' onChange={e=>setName(e.target.value)} type = 'text'  placeholder='Character...' required/>
+    <View element = {characterdata.data} character = {name} />
     </div>
     <div className='w-full h-full text-center'>
           <p>{characterdata.attributionText}</p>
         </div>
+    </form>
     </>
   )
 }
